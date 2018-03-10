37b452f085bbf6fe270b9e6c3b1ff58f0a4fa088
KrzysztofGrzywinski <krzysiekg94@gmail.com>
2018-03-10 17:15:02 +0100

    Initial commit

---
+++ Strona internetowa.aspx
@@ -0,0 +1,152 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Strona internetowa.aspx.cs" Inherits="Strona_internetowa" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Filtr pasywny by Krzysztof Grzywiński</title>
+    <link rel="stylesheet" type="text/css" href="StronaStyleSheet.css" />
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Panel ID="idPanel" runat="server" CssClass="titlePanel">
+                <h1>Politechnika Gdańska</h1>
+                <h2>Wydział Elektortechniki i Automatyki</h2>
+                <h3>Programowanie aplikacji internetowych</h3>
+                <h4>Krzysztof Grzywiński,  Nr. albumu: 149969</h4>
+            </asp:Panel>
+            <br />
+
+            <asp:Panel ID="drawingPanel" runat="server" GroupingText="Rysunek schematu ideowego filtru pasywnego" CssClass="Panel">
+                <asp:Image ID="filter" runat="server" AlternateText="Brak odpowiedniego rysunku" CssClass="Image" Width="524px" Height="198px" src="układFiltr.png" />
+            </asp:Panel>
+            <br />
+
+            <asp:Panel ID="aboutElementsPanel" runat="server" GroupingText="Opis elementów ukladu" CssClass="Panel">
+                <asp:Label ID="labAboutU1" runat="server" Text="U1 - napięcie zasilania/wejściowe filtru"></asp:Label>
+                <br />
+                <asp:Label ID="labAboutU2" runat="server" Text="U2 - napięcie wyjściowe filtru"></asp:Label>
+                <br />
+                <asp:Label ID="labAboutL" runat="server" Text="L - cewka"></asp:Label>
+                <br />
+                <asp:Label ID="labAboutC" runat="server" Text="C - kondensator"></asp:Label>
+                <br />
+                <asp:Label ID="labAboutR1" runat="server" Text="R1 - rezystor"></asp:Label>
+                <br />
+                <asp:Label ID="labAboutR2" runat="server" Text="R2 - rezystor"></asp:Label>
+            </asp:Panel>
+            <br />
+
+            <asp:Panel ID="elemParamPanel" runat="server" GroupingText="Edycja paramterów filtru" CssClass="Panel">
+                <asp:Label ID="labR1" runat="server" Text="Rezystor R1 [Ohm]:"></asp:Label>
+                <asp:TextBox ID="txtR1" runat="server" Text="1" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="labR2" runat="server" Text="Rezystor R2 [Ohm]:"></asp:Label>
+                <asp:TextBox ID="txtR2" runat="server" Text="100" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="labL" runat="server" Text="Cewka L [mH]:"></asp:Label>
+                <asp:TextBox ID="txtL" runat="server" Text="3" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="labC1" runat="server" Text="Kondensator C [uF]:"></asp:Label>
+                <asp:TextBox ID="txtC" runat="server" Text="100" CssClass="TextBox"></asp:TextBox>
+            </asp:Panel>
+
+            <br />
+            <asp:Panel ID="voltageParamPanel" runat="server" CssClass="Panel" GroupingText="Edycja paramterów napięcia zasilającego U1 oraz zakresu częstotliwości">
+                <asp:Label ID="voltU1" runat="server" Text="Amplituda napięcia U1 [V]:"></asp:Label>
+                <asp:TextBox ID="txtVoltU1" runat="server" Text="100" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="freqU1" runat="server" Text="Częstotliwość napięcia zasilającego [Hz]:"></asp:Label>
+                <asp:TextBox ID="txtFreqU1" runat="server" Text="50" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="minRangeFreq" runat="server" Text="Dolna granica zakresu częstotliwości f [Hz]:"></asp:Label>
+                <asp:TextBox ID="txtMinRangeFreq" runat="server" Text="1" CssClass="TextBox"></asp:TextBox>
+                <br />
+                <asp:Label ID="maxRangeFreq" runat="server" Text="Górna granica zakresu częstotliwości f [Hz]:"></asp:Label>
+                <asp:TextBox ID="txtMaxRangeFreq" runat="server" Text="10000" CssClass="TextBox"></asp:TextBox>
+            </asp:Panel>
+            <br />
+            <asp:Button ID="drawButton" runat="server" Text="Rysuj przebiegi" CssClass="Button" OnClick="drawButton_Click"  />
+            <br />
+            <asp:Label ID="messageLabel" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+        <br />
+
+        <asp:MultiView runat="server" ID="MultiView1" ActiveViewIndex="0">
+            <asp:View ID="widmoAmplitudowe" runat="server">
+                <asp:Label ID="labWidomoAmplitudowe1" runat="server" Text="Widmo amplitudowe" CssClass="Inactive"></asp:Label>
+                <asp:LinkButton ID="labWidmoFazowe1" runat="server" Text="Widmo fazowe" CssClass="Active" OnClick="drawButton_Click" CommandName="WidmoFazowe" OnCommand="multiView_Command"></asp:LinkButton>
+                <asp:LinkButton ID="labObwiedniaPradu1" runat="server" Text="Obwiednia prądu zasilajacego" CssClass="Active" OnClick="drawButton_Click" CommandName="ObwiedniaPradu" OnCommand="multiView_Command"></asp:LinkButton>
+                <br />
+                <br />
+                <asp:Panel ID="chartsPanel1" runat="server"  CssClass="Panel" >
+                    <asp:Chart ID="Chart1" runat="server" Width="305px" BackImage="/background2.png">
+                        <Series>
+                            <asp:Series Name="Series1">
+                            </asp:Series>
+                        </Series>
+                        <ChartAreas>
+                            <asp:ChartArea Name="ChartArea1">
+                            </asp:ChartArea>
+                        </ChartAreas>
+                        <Legends>
+                            <asp:Legend Name="Legend1"></asp:Legend>
+                        </Legends>
+                    </asp:Chart>
+                </asp:Panel>
+            </asp:View>
+            <asp:View ID="widmoFazowe" runat="server">
+                <asp:LinkButton ID="labWidmoAmplitudowe2" runat="server" Text="Widmo amplitudowe" CssClass="Active" OnClick="drawButton_Click" CommandName="WidmoAmplitudowe" OnCommand="multiView_Command"></asp:LinkButton>
+                <asp:Label ID="labWidmoFazowe2" runat="server" Text="Widmo fazowe" CssClass="Inactive"></asp:Label>
+                <asp:LinkButton ID="labObwiedniaPradu2" runat="server" Text="Obwiednia prądu zasilajacego" CssClass="Active" OnClick="drawButton_Click" CommandName="ObwiedniaPradu" OnCommand="multiView_Command"></asp:LinkButton>
+                <br />
+                <br />
+                <asp:Panel ID="chartsPanel2" runat="server" CssClass="Panel" >
+                    <asp:Chart ID="Chart2" runat="server" BackImage="/background2.png">
+                        <Series>
+                            <asp:Series Name="Series1">
+                            </asp:Series>
+                        </Series>
+                        <ChartAreas>
+                            <asp:ChartArea Name="ChartArea1">
+                            </asp:ChartArea>
+                        </ChartAreas>
+                        <Legends>
+                            <asp:Legend Name="Legend1"></asp:Legend>
+                        </Legends>
+                    </asp:Chart>
+                </asp:Panel>
+            </asp:View>
+
+            <asp:View ID="obwiedniaPradu" runat="server">
+                <asp:LinkButton ID="labWidmoAmplitudowe3" runat="server" Text="Widmo amplitudowe" CssClass="Active" OnClick="drawButton_Click" CommandName="WidmoAmplitudowe" OnCommand="multiView_Command"></asp:LinkButton>
+                <asp:LinkButton ID="labWidmoFazowe3" runat="server" Text="Widmo fazowe" CssClass="Active" OnClick="drawButton_Click" CommandName="WidmoFazowe" OnCommand="multiView_Command"></asp:LinkButton>
+                <asp:Label ID="labObwiedniaPradu3" runat="server" Text="Obwiednia prądu zasilajacego" CssClass="Inactive" ></asp:Label>
+                <br />
+                <br />
+                <asp:Panel ID="chartPanel3" runat="server" CssClass="Panel">
+                    <asp:Chart ID="Chart3" runat="server" BackImage="/background2.png">
+                        <Series>
+                            <asp:Series Name="Series1">
+                            </asp:Series>
+                        </Series>
+                        <ChartAreas>
+                            <asp:ChartArea Name="ChartArea1">
+                            </asp:ChartArea>
+                        </ChartAreas>
+                        <Legends>
+                            <asp:Legend Name="Legend1"></asp:Legend>
+                        </Legends>
+                    </asp:Chart>
+                </asp:Panel>
+            </asp:View>
+        </asp:MultiView>
+        <br />
+    </form>
+</body>
+</html>
---
+++ Strona internetowa.aspx.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.DataVisualization.Charting;
+using System.Numerics;
+using System.Web.UI.WebControls;
+
+public partial class Strona_internetowa : System.Web.UI.Page
+{
+    private double Am = 0; //amplituda napięcia zasilającego
+    private double fs = 0; //częstotliwośc napięcia zasilającego
+    private double L = 0; //wartość indukcyjności cewki w mH
+    private double C = 0; //wartość pojemności kondensatora w uF
+    private double R1 = 0; //wartość rezystancji rezystora R1 w Ohm
+    private double R2 = 0; //wartość rezystancji rezystora R2 w Ohm
+    private int fmin = 0; //dolna granica częstotliwośći pokazywanej
+    private int fmax = 0; //górna granica częstotliwośći pokazywanej
+    private int freq = 0;
+    private double omega = 0, omega1=0;
+    private Complex Z1;
+    private Complex Z2;
+    private Complex R1z;
+    private Complex R2z;
+    private Complex Xlz;
+    private Complex Xcz;
+    private Complex ku;
+    private Complex Iwe;
+
+
+    private double[,] Results; //tablica z wynikami
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            MultiView1.Visible = false;
+        }
+        else
+        {
+
+            MultiView1.Visible = true;
+            messageLabel.Text = "";
+        }
+
+        MaintainScrollPositionOnPostBack = true;
+    }
+
+
+    protected void drawButton_Click(object sender, EventArgs e)
+    {
+        if (!Double.TryParse(txtVoltU1.Text, out Am))
+        {
+            messageLabel.Text = "Błedna wartość amplitudy";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (Am <= 0)
+        {
+            messageLabel.Text = "Wartość amplitudy nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!Double.TryParse(txtFreqU1.Text, out fs))
+        {
+            messageLabel.Text = "Błedna wartość czestotliwosci";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (fs <= 0)
+        {
+            messageLabel.Text = "Wartość częstotliwości nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!Double.TryParse(txtL.Text, out L))
+        {
+            messageLabel.Text = "Błedna wartość indukcyjności C";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (L < 0)
+        {
+            messageLabel.Text = "Wartość indukcyjności nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!Double.TryParse(txtC.Text, out C))
+        {
+            messageLabel.Text = "Błedna wartość pojemności C";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (C < 0)
+        {
+            messageLabel.Text = "Wartość pojemności nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!Double.TryParse(txtR1.Text, out R1))
+        {
+            messageLabel.Text = "Błedna wartość rezystancji R1";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (R1 < 0)
+        {
+            messageLabel.Text = "Wartość rezystancji R1 nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!Double.TryParse(txtR2.Text, out R2))
+        {
+            messageLabel.Text = "Błedna wartość rezystancji R2";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (R2 < 0)
+        {
+            messageLabel.Text = "Wartość rezystancji R2 nie moze byc ujemna";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!int.TryParse(txtMinRangeFreq.Text, out fmin))
+        {
+            messageLabel.Text = "Błedna wartość dolnego zakresu częstotliwości f";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (fmin <= 0)
+        {
+            messageLabel.Text = "Wartość częstotliwości nie moze byc ujemna lub równa 0";
+            MultiView1.Visible = false;
+            return;
+        }
+        if (!int.TryParse(txtMaxRangeFreq.Text, out fmax))
+        {
+            messageLabel.Text = "Błedna wartość górnego zakresu częstotliwości f";
+            MultiView1.Visible = false;
+            return;
+        }
+        else if (fmax <= 0)
+        {
+            messageLabel.Text = "Wartość częstotliwości nie moze byc ujemna lub równa 0";
+            MultiView1.Visible = false;
+            return;
+        }
+
+        Results = new double[fmax, 5];
+        for (int i = 0; i <= fmax - fmin; i++)
+        {
+            freq = i + 1;
+
+            omega = 2 * Math.PI * freq;
+            omega1 = 2 * Math.PI * fs;
+            R1z = new Complex(R1, 0);
+            R2z = new Complex(R2, 0);
+            Xlz = new Complex(0, omega * (L / 1000));
+            Xcz = new Complex(0, -1 / (omega * (C / 1000000)));
+            Z1 = Complex.Add(R2z, Xcz);
+            Z2 = Complex.Divide(Complex.Multiply(R1z, Z1), Complex.Add(R1z, Z1));
+            ku = Complex.Multiply(Complex.Divide(Z2, Complex.Add(Xlz, Z2)), Complex.Divide(R2z, Complex.Add(R2, Xcz)));
+            Iwe = Complex.Divide(Am, Complex.Add(Z2, Xlz));
+
+            Results[i, 0] = freq;
+            Results[i, 1] = ku.Magnitude;
+            Results[i, 2] = ku.Phase;
+            Results[i, 3] = Iwe.Magnitude;
+            Results[i, 4] = -Iwe.Magnitude;
+        }
+
+        Draw();
+
+    }
+
+    private void Draw()
+    {
+        DataTable table, table2, table3;
+        DataView dView;
+ 
+        table = new DataTable();
+        table2 = new DataTable();
+        table3 = new DataTable();
+        DataColumn column;
+        DataRow row;
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Częstotliwość";
+        table.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Widmo amplitudowe";
+        table.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Częstotliwość";
+        table2.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Widmo fazowe";
+        table2.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Częstotliwość";
+        table3.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "Obwiednia prądu";
+        table3.Columns.Add(column);
+
+        column = new DataColumn();
+        column.DataType = System.Type.GetType("System.Double");
+        column.ColumnName = "-Obwiednia prądu";
+        table3.Columns.Add(column);
+
+
+        for (int i = 0; i <= fmax - fmin; i++)
+        {
+            row = table.NewRow();
+            row["Częstotliwość"] = Results[i, 0];
+            row["Widmo amplitudowe"] = Results[i, 1];
+            table.Rows.Add(row);
+        }
+        dView = new DataView(table);
+        Chart1.Series.Clear();
+        Chart1.DataBindTable(dView, "Częstotliwość");
+        Chart1.Series["Widmo amplitudowe"].ChartType = SeriesChartType.Line;
+        Chart1.ChartAreas[0].AxisX.LabelStyle.Format = "{#0}";
+        Chart1.Height = 600;
+        Chart1.Width = 1500;
+        Chart1.ChartAreas[0].BackColor = System.Drawing.Color.Azure;
+        Chart1.BackColor = System.Drawing.Color.SeaShell;
+        Chart1.ChartAreas[0].AxisX.LineWidth = 3;
+        Chart1.ChartAreas[0].AxisX.Title = "f [Hz]";
+        Chart1.ChartAreas[0].AxisY.Title = "ku [dB]";
+        Chart1.ChartAreas[0].AxisX.TitleFont = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+        Chart1.ChartAreas[0].AxisX.LogarithmBase = 10;
+        Chart1.ChartAreas[0].AxisX.IsLogarithmic = true;
+        Chart1.ChartAreas[0].AxisY.TitleFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+        Chart1.Titles.Add("Widmo amplitudowe");
+        Chart1.Titles[0].Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold);
+        Chart1.Titles[0].ForeColor = System.Drawing.Color.Black;
+        Chart1.Legends[0].DockedToChartArea = Chart1.ChartAreas[0].Name;
+        Chart1.Legends[0].Docking = Docking.Right;
+        Chart1.ChartAreas[0].AxisX.Minimum = fmin;
+        Chart1.ChartAreas[0].AxisX.Maximum = fmax;
+
+        for (int i = 0; i <= fmax - fmin; i++)
+        {
+            row = table2.NewRow();
+            row["Częstotliwość"] = Results[i, 0];
+            row["Widmo fazowe"] = Results[i, 2];
+            table2.Rows.Add(row);
+        }
+        dView = new DataView(table2);
+        Chart2.Series.Clear();
+        Chart2.DataBindTable(dView, "Częstotliwość");
+        Chart2.Series["Widmo fazowe"].ChartType = SeriesChartType.Line;
+        Chart2.ChartAreas[0].AxisX.LabelStyle.Format = "{#0}";
+        Chart2.Height = 600;
+        Chart2.Width = 1500;
+        Chart2.ChartAreas[0].BackColor = System.Drawing.Color.Azure;
+        Chart2.BackColor = System.Drawing.Color.SeaShell;
+        Chart2.ChartAreas[0].AxisX.LineWidth = 3;
+        Chart2.ChartAreas[0].AxisX.Title = "f [Hz]";
+        Chart2.ChartAreas[0].AxisY.Title = "fi [rad]";
+        Chart2.ChartAreas[0].AxisX.TitleFont = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+        Chart2.ChartAreas[0].AxisX.LogarithmBase = 10;
+        Chart2.ChartAreas[0].AxisX.IsLogarithmic = true;
+        Chart2.ChartAreas[0].AxisY.TitleFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+        Chart2.Titles.Add("Widmo fazowe");
+        Chart2.Titles[0].Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold);
+        Chart2.Titles[0].ForeColor = System.Drawing.Color.Black;
+        Chart2.Legends[0].DockedToChartArea = Chart2.ChartAreas[0].Name;
+        Chart2.Legends[0].Docking = Docking.Right;
+        Chart2.ChartAreas[0].AxisX.Minimum = fmin;
+        Chart2.ChartAreas[0].AxisX.Maximum = fmax;
+
+        for (int i = 0; i <= fmax - fmin; i++)
+        {
+            row = table3.NewRow();
+            row["Częstotliwość"] = Results[i, 0];
+            row["Obwiednia prądu"] = Results[i, 3];
+            row["-Obwiednia prądu"] = Results[i, 4];
+            table3.Rows.Add(row);
+        }
+        dView = new DataView(table3);
+        Chart3.Series.Clear();
+        Chart3.DataBindTable(dView, "Częstotliwość");
+        Chart3.Series["Obwiednia prądu"].ChartType = SeriesChartType.Line;
+        Chart3.Series["-Obwiednia prądu"].ChartType = SeriesChartType.Line;
+        Chart3.ChartAreas[0].AxisX.LabelStyle.Format = "{#0}";
+        Chart3.Height = 600;
+        Chart3.Width = 1500;
+        Chart3.ChartAreas[0].BackColor = System.Drawing.Color.Azure;
+        //Chart3.BackImage = "~/backround2.png";
+        Chart3.ChartAreas[0].AxisX.LineWidth = 3;
+        Chart3.ChartAreas[0].AxisX.Title = "f [Hz]";
+        Chart3.ChartAreas[0].AxisY.Title = "I [A]";
+        Chart3.ChartAreas[0].AxisX.TitleFont = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
+        Chart3.ChartAreas[0].AxisX.LogarithmBase = 10;
+        Chart3.ChartAreas[0].AxisX.IsLogarithmic = true;
+        Chart3.ChartAreas[0].AxisY.TitleFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+        Chart3.Titles.Add("Obwiednia prądu zasilającego");
+        Chart3.Titles[0].Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold);
+        Chart3.Titles[0].ForeColor = System.Drawing.Color.Black;
+        Chart3.Legends[0].DockedToChartArea = Chart2.ChartAreas[0].Name;
+        Chart3.Legends[0].Docking = Docking.Right;
+        Chart3.ChartAreas[0].AxisX.Minimum = fmin;
+        Chart3.ChartAreas[0].AxisX.Maximum = fmax;
+
+    }
+
+
+    protected void multiView_Command(object sender, CommandEventArgs e)
+    {
+        switch ((string)e.CommandName)
+        {
+            case "WidmoAmplitudowe":
+                {
+
+                    MultiView1.SetActiveView(widmoAmplitudowe);
+                    break;
+
+                }
+            case "WidmoFazowe":
+                {
+                    MultiView1.SetActiveView(widmoFazowe);
+                    break;
+                }
+            case "ObwiedniaPradu":
+                {
+                    MultiView1.SetActiveView(obwiedniaPradu);
+                    break;
+                }
+
+        }
+    }
+}
